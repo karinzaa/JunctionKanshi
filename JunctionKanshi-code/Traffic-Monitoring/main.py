1509a96972866a32c9f49604d76f24587dee7fc6
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2024-02-12 14:46:11 +0700

    Changes to be committed: Add logo loader 	modified:   JunctionKanshi-code/Traffic-Monitoring/main.py

--- JunctionKanshi-code/Traffic-Monitoring/main.py
+++ JunctionKanshi-code/Traffic-Monitoring/main.py
@@ -10,6 +10,10 @@ import numpy as np
 WIDTH = 1280
 HEIGHT = 720
 
+logo = cv2.imread("static/JunctionKanshiLogo.png", cv2.IMREAD_UNCHANGED)  #Logo must is PNG with transparency
+logo = cv2.resize(logo, (50, 75))  # Resize as needed
+position = 'bottom-right' #Location as needed
+
 def overlay_logo(frame, logo, position, margin=10):
     frame_h, frame_w, _ = frame.shape
     logo_h, logo_w, _ = logo.shape
