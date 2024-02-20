36e93862d4f141a1655f6551af47b7c8e4e8df8b
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2024-02-20 10:32:55 +0700

    Changes to be committed: Changes in Traffic-Monitoring windows. 	modified:   JunctionKanshi-code/Traffic-Monitoring/main.py 	deleted:    JunctionKanshi-code/Traffic-Monitoring/static/JunctionKanshiLogo.png 	new file:   JunctionKanshi-code/Traffic-Monitoring/static/JunctionKanshiLogoLandscape.png

--- JunctionKanshi-code/Traffic-Monitoring/main.py
+++ JunctionKanshi-code/Traffic-Monitoring/main.py
@@ -16,9 +16,9 @@ WIDTH = 1280
 HEIGHT = 720
 
 # Load the logo, possibly resize and position
-logo = cv2.imread("static/JunctionKanshiLogo.png", cv2.IMREAD_UNCHANGED)  # Logo must is PNG with transparency
-logo = cv2.resize(logo, (50, 75))  # Resize as needed
-position = 'bottom-right' #Location as needed
+logo = cv2.imread("static/JunctionKanshiLogoLandscape.png", cv2.IMREAD_UNCHANGED)  # Logo must is PNG with transparency
+logo = cv2.resize(logo, (195, 50))  # Resize as needed
+position = 'bottom-right' # Location as needed
 
 # Read the mask image
 mask = cv2.imread("static/mask.png")
