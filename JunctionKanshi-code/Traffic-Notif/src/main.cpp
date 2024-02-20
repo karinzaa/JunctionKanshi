134561b71a13dc4e7a117fa47a85c232dc4de90c
rosiindahag <shina_ruki@yahoo.com>
2024-02-20 17:55:19 +0700

    initial commit

---
+++ JunctionKanshi-code/Traffic-Notif/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define MQTT_BROKER       "broker.hivemq.com"
+#define MQTT_PORT         1883
+#define HIVEMQ_USERNAME   "taist_aiot_dev"
+#define MQTT_NOTIFICATION  "taist/aiot/notification/dev_00"
+
+#define WIFI_SSID         "dtac"
+#define WIFI_PASSWORD     "notokiam"
+
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);
+  delay(100);
+  WiFi.mode(WIFI_STA);
+  delay(100);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+    }
+
+void loop() {
+  Serial.println("OK");
+}
